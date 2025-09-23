080872bdffbf3e20cbe9bfe8420381375c2d08eb
ironwally <ironwally@ratfort.de>
2025-09-23 12:32:25 +0200

    dependency update: java openjdk-21

--- release-local.sh
+++ release-local.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 # Run tests and publish local to ./local-repo
-./mvnw clean deploy -DaltDeploymentRepository=local::default::file:./local-repo \
+./mvnw clean deploy -DaltDeploymentRepository=local::file:./local-repo \
   && echo "Published to $(cd ./local-repo && pwd)"
